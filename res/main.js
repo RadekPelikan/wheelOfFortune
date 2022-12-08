3e354782e5b4caf03d34eeabe48c976efa89c8b4
Radek Pelikán <radek.pelikan@student.spsmb.cz>
2022-12-08 17:13:45 +0100

    minor changes

--- res/main.js
+++ res/main.js
@@ -1,14 +1,39 @@
-const itemElements = document.querySelectorAll("ul#items-list li");
-let lastItemElement = itemElements[itemElements.length - 1];
+const itemsListElement = document.querySelector("ul#items-list");
+let itemElements = [];
+let lastItemElement = null;
 
 const handleItemUpdate = (e) => {
-    console.log(e.target.value)
+  console.log(e.target.value);
+};
+
+const handleLastItemUpdate = (e) => {
+  addInputElement()
+  lastItemElement.addEventListener("keyup", handleLastItemUpdate)
+};
+
+const addInputElement = () => {
+  lastItemElement.removeEventListener("keyup", handleLastItemUpdate)
+  const listItemElement = document.createElement("li")
+  listItemElement.innerHTML = `
+      <input type="text">
+  `
+  itemsListElement.appendChild(listItemElement)
+  updateItemElements()
 }
 
-itemElements.forEach((item, i) => {
-    item.addEventListener("keyup", handleItemUpdate)
-})
+const updateItemElements = () => {
+  itemElements.slice(0, -1).forEach((item, i) => {
+    item.addEventListener("keyup", handleItemUpdate);
+  }); 
+  itemElements = [...itemsListElement.querySelectorAll("li")];
+  lastItemElement = itemElements[itemElements.length - 1]
+};
 
-// lastItemElement.addEventListener("keyup", (e) => {
-//     console.log(e.target.value)
-// }, false);
+
+window.onload = () => {
+  updateItemElements();
+  itemElements.slice(0, -1).forEach((item, i) => {
+    item.addEventListener("keyup", handleItemUpdate);
+  }); 
+  lastItemElement.addEventListener("keyup", handleLastItemUpdate)
+};
